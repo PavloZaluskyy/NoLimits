9f5737529ce1888a49c7db243acf0ce77f35d85a
PavloZaluskyy <pavlozaluskiy@gmail.com>
2020-08-08 18:36:11 +0300

    improve player

--- build/script/index.js
+++ build/script/index.js
@@ -6,7 +6,39 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
+var count = 1,
+    togglePlay = true;
+var audio = new Audio();
 renderSongs();
+document.querySelector(".iconPrevious").addEventListener('click', function () {
+  prev();
+  showPreview();
+  play();
+});
+document.querySelector(".iconNext").addEventListener('click', function () {
+  next();
+  showPreview();
+  play();
+});
+document.querySelector(".iconPlay").addEventListener('click', function () {
+  togglePlay = !togglePlay;
+
+  if (togglePlay !== true) {
+    removeClass(".playOrPause", ".iconPlay");
+    document.querySelector(".playOrPause").classList.add("iconPause");
+    play();
+  } else {
+    removeClass(".playOrPause", ".iconPause");
+    document.querySelector(".playOrPause").classList.add("iconPlay");
+    pause();
+  }
+});
+document.querySelector(".m_playlist").addEventListener('click', function (event) {
+  count = +event.target.dataset.songid + 1;
+  prev();
+  showPreview();
+  play();
+});
 
 function renderSongs() {
   var renderNameSong = "",
@@ -21,8 +53,8 @@ function renderSongs() {
 
       if (key.id === 1) {
         renderImg.style.background = "url(/data/player/img/".concat(key.urlImg, ")");
-        renderNameSong += "<li data-id=\"".concat(key.id, "\" class=\"active songs\">").concat(key.nameSong, "</li>");
-      } else renderNameSong += "<li data-id=\"".concat(key.id, "\" class=\"songs\">").concat(key.nameSong, "</li>");
+        renderNameSong += "<li data-songId=\"".concat(key.id, "\" class=\"active songs\">").concat(key.nameSong, "</li>");
+      } else renderNameSong += "<li data-songId=\"".concat(key.id, "\" class=\"songs\">").concat(key.nameSong, "</li>");
     }
   } catch (err) {
     _iterator.e(err);
@@ -31,14 +63,56 @@ function renderSongs() {
   }
 
   document.querySelector('.m_playlist').innerHTML = renderNameSong;
+  audio.src = "../data/player/songs/".concat(dataSongs[0].urlSong);
 }
 
-document.querySelector('.m_playlist').addEventListener('click', function (event) {
-  console.log(+event.target.dataset.id - 1);
-  playSong(dataSongs[+event.target.dataset.id - 1]);
-});
+function removeClass(yourSelectClass, removeYourClass) {
+  var selectClass = document.querySelector(yourSelectClass);
+  selectClass.classList.remove(removeYourClass);
+}
 
-function playSong(url) {
-  var audio = new Audio("../data/player/songs/".concat(url.urlSong));
+function play() {
   audio.play();
+  setInterval(function () {
+    document.querySelector('#time').innerText = "".concat(Math.floor(audio.currentTime / 60), " : ").concat((audio.currentTime % 60).toFixed(0), " ");
+  }, 1000);
+}
+
+function pause() {
+  audio.pause();
+}
+
+function showPreview() {
+  var renderImg = document.querySelector('.preview');
+  renderImg.style.background = "url(/data/player/img/".concat(dataSongs[count - 1].urlImg, ")");
+}
+
+function next() {
+  removeClass("li.active", "active");
+
+  if (count < dataSongs.length) {
+    count++;
+    document.querySelector("[data-songId=\"".concat(count, "\"]")).classList.add("active");
+  } else {
+    count = 1;
+    document.querySelector("[data-songId=\"".concat(count, "\"]")).classList.add("active");
+  }
+
+  audio.src = "../data/player/songs/".concat(dataSongs[count - 1].urlSong);
+  togglePlay = false;
+}
+
+function prev() {
+  removeClass("li.active", "active");
+
+  if (count > 1) {
+    count--;
+    document.querySelector("[data-songId=\"".concat(count, "\"]")).classList.add("active");
+  } else {
+    count = dataSongs.length;
+    document.querySelector("[data-songId=\"".concat(count, "\"]")).classList.add("active");
+  }
+
+  togglePlay = false;
+  audio.src = "../data/player/songs/".concat(dataSongs[count - 1].urlSong);
 }
--- src/script/index.js
+++ src/script/index.js
@@ -1,25 +1,90 @@
+let count = 1,
+	togglePlay = true;
+const audio = new Audio();
 renderSongs();
+document.querySelector(".iconPrevious").addEventListener('click', () => {prev(); showPreview(); play()});
+document.querySelector(".iconNext").addEventListener('click', () => {next(); showPreview(); play()});
+document.querySelector(".iconPlay").addEventListener('click', ()=>{
+	togglePlay = !togglePlay;
+	if(togglePlay !==true){
+		removeClass(".playOrPause", ".iconPlay");
+		document.querySelector(`.playOrPause`).classList.add("iconPause");
+		play();
+	}else{
+		removeClass(".playOrPause", ".iconPause");
+		document.querySelector(`.playOrPause`).classList.add("iconPlay");
+		pause();
+	}
+
+});
+document.querySelector(".m_playlist").addEventListener('click', (event) => {
+	
+	count = +event.target.dataset.songid + 1;
+	prev(); showPreview(); play()
+
+});
+
 function renderSongs(){
 	let renderNameSong = "",
 		renderImg = document.querySelector('.preview');
 	for(let key of dataSongs){
 		if(key.id === 1){
 		renderImg.style.background = `url(/data/player/img/${key.urlImg})`;
-		renderNameSong += `<li data-id="${key.id}" class="active songs">${key.nameSong}</li>`;
+		renderNameSong += `<li data-songId="${key.id}" class="active songs">${key.nameSong}</li>`;
 		
 		}
-		else renderNameSong += `<li data-id="${key.id}" class="songs">${key.nameSong}</li>`;
+		else renderNameSong += `<li data-songId="${key.id}" class="songs">${key.nameSong}</li>`;
 	}
 	document.querySelector('.m_playlist').innerHTML = renderNameSong;
+	audio.src = `../data/player/songs/${dataSongs[0].urlSong}`;
 }
 
-document.querySelector('.m_playlist').addEventListener('click', event => {
-		console.log(+event.target.dataset.id - 1)
-		playSong(dataSongs[+event.target.dataset.id - 1]);
-	
-});
+function removeClass(yourSelectClass, removeYourClass){
+	let selectClass = document.querySelector(yourSelectClass);
+	selectClass.classList.remove(removeYourClass);
+}
 
-function playSong(url){
-	const audio = new Audio(`../data/player/songs/${url.urlSong}`);
+
+
+function play(){
 	audio.play();
+	setInterval(()=>{
+
+		document.querySelector('#time').innerText = `${Math.floor(audio.currentTime / 60)} : ${(audio.currentTime % 60).toFixed(0)} `;
+	}, 1000)
 }
+function pause(){
+	audio.pause();
+}
+
+function showPreview(){
+	const renderImg = document.querySelector('.preview');
+	renderImg.style.background = `url(/data/player/img/${dataSongs[count-1].urlImg})`;
+}
+
+function next() {
+	removeClass("li.active", "active");
+	if(count < dataSongs.length){
+		count++;
+		document.querySelector(`[data-songId="${count}"]`).classList.add("active");
+	}else{
+		count = 1;
+		document.querySelector(`[data-songId="${count}"]`).classList.add("active");
+	}
+	audio.src = `../data/player/songs/${dataSongs[count-1].urlSong}`;
+	togglePlay = false;
+}
+function prev() {
+	removeClass("li.active","active");
+	if(count > 1){
+		count--;
+		document.querySelector(`[data-songId="${count}"]`).classList.add("active");
+	}else{
+		count = dataSongs.length;
+		document.querySelector(`[data-songId="${count}"]`).classList.add("active");
+	}
+	togglePlay = false;
+	audio.src = `../data/player/songs/${dataSongs[count-1].urlSong}`;
+}
+
+
