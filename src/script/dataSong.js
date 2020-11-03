bd63e2bc7881a4bc4e79681948891e4f47d5a4f5
PavloZaluskyy <pavlozaluskiy@gmail.com>
2020-11-03 15:53:11 +0200

    finish

--- src/script/dataSong.js
+++ src/script/dataSong.js
@@ -1,74 +1,86 @@
 const dataSongs = [
 	{
 		"id": 1,
-		"nameSong": "Heaven",
-		"urlSong": "3 Doors Down - Heaven.mp3",
-		"urlImg": "3DD.jpg"
+		"nameSong": "Porokh",
+		"urlSong": "No Limits - Porokh.mp3",
+		"urlImg": "artworks-000135443868-t4duc0-t200x200.jpg"
 	},
 	{
 		"id": 2,
-		"nameSong": "Inside Of Me",
-		"urlSong": "3 Doors Down - Inside Of Me.mp3",
-		"urlImg": "3DD.jpg"
+		"nameSong": "Будемо разом",
+		"urlSong": "No Limits - Будемо разом.mp3",
+		"urlImg": "artworks-000101681255-ds66p3-t200x200.jpg"
 	},
 	{
 		"id": 3,
-		"nameSong": "Here I Am",
-		"urlSong": "Asking Alexandria - Here I Am.mp3",
-		"urlImg": "AA.png"
+		"nameSong": "Бути з Тобою",
+		"urlSong": "No Limits - Бути з Тобою.mp3",
+		"urlImg": "artworks-000101681255-ds66p3-t200x200.jpg"
 	},
 	{
 		"id": 4,
-		"nameSong": "Nothing To Lose ",
-		"urlSong": "Billy Talent - Nothing To Lose  .mp3",
-		"urlImg": "BT.jpg"
+		"nameSong": "Зі мною ",
+		"urlSong": "No Limits - З мною.mp3",
+		"urlImg": "artworks-000101681255-ds66p3-t200x200.jpg"
 	},
 	{
 		"id": 5,
-		"nameSong": "Rebel Love Song",
-		"urlSong": "Black Veil Brides - Rebel Love Song.mp3",
-		"urlImg": "BVB.jpg"
+		"nameSong": "Ми ті хто ми.mp3",
+		"urlSong": "No Limits - Ми т хто ми.mp3",
+		"urlImg": "artworks-000101681255-ds66p3-t200x200.jpg"
 	},
 	{
 		"id": 6,
-		"nameSong": "Every Monday",
-		"urlSong": "Brunettes Shoot Blondes - Every Monday.mp3",
-		"urlImg": "nan.png"
+		"nameSong": "Не така як всі",
+		"urlSong": "No Limits - Не така як вс.mp3",
+		"urlImg": "artworks-000101681255-ds66p3-t200x200.jpg"
 	},
 	{
 		"id": 7,
-		"nameSong": "I Don't Know",
-		"urlSong": "Brunettes Shoot Blondes - I Don't Know.mp3",
-		"urlImg": "nan.png"
+		"nameSong": "Пацифік (Слова - С. Жадан)",
+		"urlSong": "No Limits - Пацифк (Слова - С. Жадан).mp3",
+		"urlImg": "artworks-000101681255-ds66p3-t200x200.jpg"
 	},
 	{
 		"id": 8,
-		"nameSong": "Mystery",
-		"urlSong": "Dead by April - Mystery.mp3",
-		"urlImg": "DBA.jpg"
+		"nameSong": "Різдвяна (Bonus Track)",
+		"urlSong": "No Limits - Рздвяна (Bonus Track).mp3",
+		"urlImg": "artworks-000101681255-ds66p3-t200x200.jpg"
 	},
 	{
 		"id": 9,
-		"nameSong": "America",
-		"urlSong": "Deuce - America.mp3",
-		"urlImg": "HU.png"
+		"nameSong": "Цей день [Single 2015]",
+		"urlSong": "No Limits  Цей день [Single 2015].mp3",
+		"urlImg": "artworks-000115067854-vv9e0b-t200x200.jpg"
 	},
 	{
 		"id": 10,
-		"nameSong": "Thank You",
-		"urlSong": "Deuce - Thank You.mp3",
-		"urlImg": "HU.png"
+		"nameSong": "Не я",
+		"urlSong": "Не я.mp3",
+		"urlImg": "artworks-000220215005-c8m03v-t200x200.jpg"
 	},
 	{
 		"id": 11,
-		"nameSong": "Рull Ме Undеr",
-		"urlSong": "Deuce - Рull Ме Undеr.mp3",
-		"urlImg": "HU.png"
+		"nameSong": "Планета",
+		"urlSong": "Планета.mp3",
+		"urlImg": "artworks-000213428385-lkajjw-t200x200.jpg"
 	},
 	{
 		"id": 12,
-		"nameSong": "Sing",
-		"urlSong": "Dope - Sing.mp3",
-		"urlImg": "nan.png"
+		"nameSong": "Свою свободу знайдем",
+		"urlSong": "Свою свободу знайдем.mp3",
+		"urlImg": "artworks-000215420239-0r5ikt-t200x200.jpg"
+	},
+	{
+		"id": 13,
+		"nameSong": "Хітяра",
+		"urlSong": "Хтяра.mp3",
+		"urlImg": "artworks-000220215088-yz5qrj-t200x200.jpg"
+	},
+	{
+		"id": 14,
+		"nameSong": "Чуєш небо",
+		"urlSong": "No Limits - Чуш небо.mp3",
+		"urlImg": "artworks-000229796893-9k4bon-t200x200.jpg"
 	}
 ]
